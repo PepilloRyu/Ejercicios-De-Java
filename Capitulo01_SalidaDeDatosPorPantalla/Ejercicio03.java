5de400ad619b23a846474459003edc3b666a375c
Pepillo Ryu <josemtzh1620@hotmail.com>
2025-09-03 23:28:03 -0600

    Update Ejercicio03.java

--- Capitulo01_SalidaDeDatosPorPantalla/Ejercicio03.java
+++ Capitulo01_SalidaDeDatosPorPantalla/Ejercicio03.java
@@ -15,4 +15,25 @@
  * book libro
  * pyramid pirámid
  * 
- */
+*/
+
+public class Ejercicio03
+{
+    public static void main(String[] args) 
+    {
+        System.out.printf("%-15s %s%n","Computer","Ordenador");    
+        System.out.printf("%-15s %s%n","Student","Alumno");
+        System.out.printf("%-15s %s%n","Cat","Gato");
+        System.out.printf("%-15s %s%n","Penguin","Pingüino");
+        System.out.printf("%-15s %s%n","Machine","Maquina");
+        System.out.printf("%-15s %s%n","Nature","Naturaleza");
+        System.out.printf("%-15s %s%n","Light","Luz");
+        System.out.printf("%-15s %s%n","Green","Verde");
+        System.out.printf("%-15s %s%n","Book","Libro");
+        System.out.printf("%-15s %s%n","Pyramid","Pirámide");
+    }
+}
+/*
+%s -> significa que va a sustituir una cadena. 
+%n -> es un salto de línea.
+*/
