cc4ce4442c3a2a839d486ed0c0817dcb3b82b93d
Pepillo Ryu <josemtzh1620@hotmail.com>
2025-09-05 01:20:20 -0600

    Add files via upload

---
+++ Capitulo02_Variables/Ejercicio03.java
@@ -0,0 +1,18 @@
+/*
+ * Ejercicio 3
+ * Crea las variables nombre, escuela y telefono y asígnales los valores correspon-
+ * dientes. Muestra los valores de esas variables por pantalla de tal forma que el
+ * resultado del programa sea el mismo que en el ejercicio 2
+*/
+public class Ejercicio03
+{
+    public static void main(String[] args) 
+    {
+        String nombre = "Jose Carlos Hernández Martinez";
+        String escuela = "Escuela Superior de Cómputo";
+        String telefono = "5527569826";  
+        System.out.println("Nombre : " + nombre);
+        System.out.println("Escuela : " + escuela);
+        System.out.println("Telefono : " + telefono );
+    }
+}
