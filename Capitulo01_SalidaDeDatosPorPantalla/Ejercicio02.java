416a746352742efdbb0d46fede6b70a90400bb64
Pepillo Ryu <josemtzh1620@hotmail.com>
2025-09-04 19:03:09 -0600

    Update Ejercicio02.java

--- Capitulo01_SalidaDeDatosPorPantalla/Ejercicio02.java
+++ Capitulo01_SalidaDeDatosPorPantalla/Ejercicio02.java
@@ -1,6 +1,6 @@
 /*
  * Ejercicio 2
- * Modifica el programa anterior para que además se muestre tu dirección y
+ * Modifica el programa anterior para que además se muestre tu escuela y
  * tu número de teléfono. Asegúrate de que los datos se muestran en líneas
  * separada 
  */
@@ -10,7 +10,8 @@ public class Ejercicio02
     public static void main(String[] args)
     {
         System.out.println("Jose Carlos Hernández Martinez");
-        System.out.println("Emiliano Zapata #5");
+        System.out.println("Escuela Superior de Cómputo");
         System.err.println("55275699826");
     }
 }
+
