c496a12d1f203b31064ae84f5b7c561ddea8e053
Pepillo Ryu <josemtzh1620@hotmail.com>
2025-09-05 00:44:32 -0600

    Update Ejercicio01.java

---
+++ Capitulo02_Variables/Ejercicio01.java
@@ -0,0 +1,20 @@
+/*
+ * Ejercicio 1
+ * Escribe un programa en el que se declaren las variables enteras x e y. Asígnales
+ * los valores 144 y 999 respectivamente. A continuación, muestra por pantalla
+ * el valor de cada variable, la suma, la resta, la división y la multiplicación.
+*/
+
+public class Ejercicio01
+{
+    public static void main(String[] args) 
+    {
+        int x = 144;
+        int y = 999;
+        int resultado;
+        resultado = x + y;
+        System.out.println("Valor x : " + x);
+        System.out.println("Valor y : " + y);
+        System.out.println("Resultado : " + resultado);
+    }
+}
