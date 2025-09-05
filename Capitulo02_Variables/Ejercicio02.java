2129c56003b2e553ebd6efb5b25891fd25eda577
Pepillo Ryu <josemtzh1620@hotmail.com>
2025-09-05 01:00:27 -0600

    Add files via upload

---
+++ Capitulo02_Variables/Ejercicio02.java
@@ -0,0 +1,13 @@
+/*
+ * Ejercicio 2
+ * Crea la variable nombre y asígnale tu nombre completo. Muestra su valor por
+ * pantalla.
+*/
+public class Ejercicio02
+{
+    public static void main(String[] args) 
+    {
+        String miNombre = "Jose Carlos Hernandez Martines";
+        System.out.println("Mi nombre es : "+ miNombre);
+    }
+}
